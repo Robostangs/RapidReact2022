8480d5107ac1d69697e2d63269728cf9f41e89ec
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-01-12 20:27:23 -0500

    Add deadzone to utils

--- src/main/java/frc/robot/Utils.java
+++ src/main/java/frc/robot/Utils.java
@@ -2,4 +2,25 @@ package frc.robot;
 
 public final class Utils {
     
+    public static double deadzone(double input) {
+        return deadzone(input, 2);
+    }
+
+    public static double deadzone(double input, double power) {
+        return deadzone(input, power, -1, -0.1, 0.1, 1);
+    }
+
+    public static double deadzone(double input, double power, double lower_maxzone, double lower_deadzone, double higher_deadzone, double higher_maxzone) {
+        if (input <= lower_maxzone) {
+            return -1;
+        } else if (lower_maxzone < input && input < lower_deadzone) {
+            return Math.pow((-input + lower_deadzone) / (lower_deadzone - lower_maxzone), power);
+        } else if (lower_deadzone <= input && input <= higher_deadzone) {
+            return 0;
+        } else if (higher_deadzone < input && input < higher_maxzone) {
+            return Math.pow((input - higher_deadzone) / (higher_maxzone - higher_deadzone), power);
+        } else {
+            return 1;
+        }
+    }
 }
