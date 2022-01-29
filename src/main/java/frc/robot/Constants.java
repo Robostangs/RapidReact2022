1181479778d10ea606841acc15eb916879d669d8
Saket Kulkarni <31022476+SaketKulkarni121@users.noreply.github.com>
2022-01-29 14:38:02 -0500

    Added Climber Class
    Added the climber class, not any commands yet

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static class Climber {
+        public static final int rotationMotorID = 0;
+        public static final int leftClawID = 1;
+        public static final int rightClawID = 2;
+        public static final int leftClawLockID = 3;
+        public static final int rightClawLockID = 4;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,84 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Climber extends SubsystemBase{
+    
+    public static Climber instance;
+    private TalonFX m_rotationMotor;  
+    private CANSparkMax m_leftClaw, m_rightClaw;
+    private Servo m_leftClawLock, m_rightClawLock;
+
+
+    public static Climber getInstance() {
+        if(instance == null) {
+            instance = new Climber();
+        }
+        return instance;
+    }
+
+    public Climber() {
+        m_rotationMotor = new TalonFX(Constants.Climber.rotationMotorID);
+        
+        m_leftClaw = new CANSparkMax(Constants.Climber.leftClawID, MotorType.kBrushless);
+        m_rightClaw = new CANSparkMax(Constants.Climber.rightClawID, MotorType.kBrushless);
+        
+        m_leftClawLock = new Servo(Constants.Climber.leftClawLockID);
+        m_rightClawLock = new Servo(Constants.Climber.rightClawLockID);
+    }
+
+    public void setRotationMotorSpeed(double power) {
+        m_rotationMotor.set(ControlMode.PercentOutput, power);
+    }
+
+    public void setRotationMotorPosition(double position) {
+        m_rotationMotor.set(ControlMode.Position, position);
+    }
+
+    // public void setRotationMotorMagicPosition(double position) {
+    //     m_rotationMotor.set(ControlMode.MotionMagic, position);
+    // }
+
+    public double getRotationMotorPosition() {
+        return m_rotationMotor.getActiveTrajectoryPosition();
+    }
+
+    public void setLeftClawSpeed(double power) {
+        m_leftClaw.set(power);
+    }
+
+    public void setRightClawSpeed(double power) {
+        m_rightClaw.set(power);
+    }
+
+    public double getLeftClawPosition() {
+        return m_leftClaw.getEncoder().getPosition();
+    }
+
+    public double getRightClawPosition() {
+        return m_rightClaw.getEncoder().getPosition();
+    }
+
+    public void setRightClawLockPosition(double position) {
+        m_rightClawLock.set(position);
+    }
+
+    public void setLeftClawPosition(double position) {
+        m_leftClawLock.set(position);
+    }
+
+    public double getRightClawLockPosition() {
+        return m_rightClawLock.getPosition();
+    }
+
+    public double getLeftClawLockPosition() {
+        return m_leftClawLock.getPosition();
+    }
+}
