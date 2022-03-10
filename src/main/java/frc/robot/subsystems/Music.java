608a42c1591733a622af8bfe8c927d54bbbfc08d
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-03-10 11:54:32 -0500

    Improve music constructor

--- src/main/java/frc/robot/subsystems/Music.java
+++ src/main/java/frc/robot/subsystems/Music.java
@@ -20,27 +20,12 @@ public class Music {
         m_Orchestra = new Orchestra();
     }
 
-    public static void loadMusic(WPI_TalonFX Falcon1, WPI_TalonFX Falcon2,WPI_TalonFX Falcon3,WPI_TalonFX Falcon4,WPI_TalonFX Falcon5,WPI_TalonFX Falcon6,WPI_TalonFX Falcon7,WPI_TalonFX Falcon8,WPI_TalonFX Falcon9,WPI_TalonFX Falcon10,WPI_TalonFX Falcon11,WPI_TalonFX Falcon12,WPI_TalonFX Falcon13,WPI_TalonFX Falcon14,WPI_TalonFX Falcon15,WPI_TalonFX Falcon16,WPI_TalonFX Falcon17, String musicFilePath) {
-        m_Orchestra.addInstrument(Falcon1);
-        m_Orchestra.addInstrument(Falcon2);
-        m_Orchestra.addInstrument(Falcon3);
-        m_Orchestra.addInstrument(Falcon4);
-        m_Orchestra.addInstrument(Falcon5);
-        m_Orchestra.addInstrument(Falcon6);
-        m_Orchestra.addInstrument(Falcon7);
-        m_Orchestra.addInstrument(Falcon8);
-        m_Orchestra.addInstrument(Falcon9);
-        m_Orchestra.addInstrument(Falcon10);
-        m_Orchestra.addInstrument(Falcon11);
-        m_Orchestra.addInstrument(Falcon12);
-        m_Orchestra.addInstrument(Falcon13);
-        m_Orchestra.addInstrument(Falcon14);
-        m_Orchestra.addInstrument(Falcon15);
-        m_Orchestra.addInstrument(Falcon16);
-        m_Orchestra.addInstrument(Falcon17);
-//Music File Path Just has to be the name of the chirp file in deploy, like dynamite.chrp
+    public static void loadMusic(String musicFilePath, WPI_TalonFX... Falcons) {
+        for (WPI_TalonFX Falcon : Falcons) {
+            m_Orchestra.addInstrument(Falcon);
+        }
+        //Music File Path Just has to be the name of the chirp file in deploy, like dynamite.chrp
         m_Orchestra.loadMusic(musicFilePath);
-
         Timer.delay(2);
     }
 
