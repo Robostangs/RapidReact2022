1aaa0afaed916347d2129ab594884683eb043ae7
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-03-11 18:38:11 -0500

    Fix residue from stupid fix

--- src/main/java/frc/robot/commands/climber/ClawRight.java
+++ src/main/java/frc/robot/commands/climber/ClawRight.java
@@ -3,12 +3,12 @@ package frc.robot.commands.climber;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Climber;
 
-public class ClawRighta extends CommandBase {
+public class ClawRight extends CommandBase {
 
     private final double mPosition;
     private final Climber mClimber = Climber.getInstance();
 
-    public ClawRighta(double position) {
+    public ClawRight(double position) {
         addRequirements(mClimber);
         setName("Right Claw");
         mPosition = position;
