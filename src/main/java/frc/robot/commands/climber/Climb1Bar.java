518d388ef196ac2b647ce512c3c1ba0360200459
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-03-12 12:23:30 -0500

    Finish Climb1Bar

--- src/main/java/frc/robot/commands/climber/Climb1Bar.java
+++ src/main/java/frc/robot/commands/climber/Climb1Bar.java
@@ -1,13 +1,17 @@
 package frc.robot.commands.climber;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Climber;
 
-public class Climb1Bar extends CommandBase {
+public class Climb1Bar extends SequentialCommandGroup {
     private final Climber mClimber = Climber.getInstance();
 
     public Climb1Bar(Climber.Hand rotationHand, Climber.Hand grabHand) {
         addRequirements(mClimber);
         setName("Climb 1 Bar");
+        this.addCommands(
+            new Rotate().until(() -> (grabHand.getEngaged())),
+            new CloseHand(grabHand),
+            new OpenHand(rotationHand));
     }
 }
