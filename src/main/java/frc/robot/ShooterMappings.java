520c81bce080a21e51946c56e7ebe0496e68049c
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-03-23 15:28:13 -0400

    Remove Hood

--- src/main/java/frc/robot/ShooterMappings.java
+++ src/main/java/frc/robot/ShooterMappings.java
@@ -8,29 +8,29 @@ import frc.robot.subsystems.Shooter;
 
 public class ShooterMappings {
     public static final HashMap<Double, Shooter.State> kShooterStates;
-    private static void addEntry(double distance, double topShooterPower, double bottomShooterPower, double angle) {
-        kShooterStates.put(distance, new Shooter.State(topShooterPower, bottomShooterPower, angle));
+    private static void addEntry(double distance, double topShooterPower, double bottomShooterPower) {
+        kShooterStates.put(distance, new Shooter.State(topShooterPower, bottomShooterPower);
     }
     // distance =           {0,     56,     61,     68,     80,     84,     87,     92,     104,    108,    116,    128,    142,    177,    165},
     // topShooterPower =    {500,   2000,   2100,   2000,   2000,   2100,   2200,   2175,   2100,   2300,   2250,   2600,   2750,   2761,   2700},
     // bottomShooterPower = {500,   2000,   2000,   2300,   2350,   2400,   2400,   2459,   2500,   2586,   2600,   2990,   3163,   3000,   2900},
-    // angle =              {10,    0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      10,     10};
+    // // angle =              {10,    0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      0,      10,     10};
     static {
         kShooterStates = new HashMap<Double, Shooter.State>();
-        addEntry(-10, 0, 0, 0);
-        addEntry(0, 800, 800, 0);
-        addEntry(56, 2000, 2000, 0);
-        addEntry(61, 2100, 2000, 0);
-        addEntry(68, 2000, 2300, 0);
-        addEntry(80, 2000, 2350, 0);
-        addEntry(84, 2100, 2400, 0);
-        addEntry(87, 2200, 2400, 0);
-        addEntry(92, 2175, 2459, 0);
-        addEntry(104, 2100, 2500, 0);
-        addEntry(108, 2300, 2586, 0);
-        addEntry(116, 2250, 2600, 0);
-        addEntry(128, 2600, 2990, 0);
-        addEntry(142, 2750, 3163, 0);
+        addEntry(-10, 0, 0);
+        addEntry(0, 800, 800);
+        addEntry(56, 2000, 2000);
+        addEntry(61, 2100, 2000);
+        addEntry(68, 2000, 2300);
+        addEntry(80, 2000, 2350);
+        addEntry(84, 2100, 2400);
+        addEntry(87, 2200, 2400);
+        addEntry(92, 2175, 2459);
+        addEntry(104, 2100, 2500);
+        addEntry(108, 2300, 2586);
+        addEntry(116, 2250, 2600);
+        addEntry(128, 2600, 2990);
+        addEntry(142, 2750, 3163);
         // addEntry(177, 2761, 3000, 10);
         // addEntry(165, 2700, 2900, 10);
     };
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -12,37 +12,30 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Utils;
-import frc.robot.commands.shooter.Home;
 
 public class Shooter extends SubsystemBase {
 
     public static class State {
         public double topSpeed;
         public double bottomSpeed;
-        public double angle;
-
-        public State(double topSpeed, double bottomSpeed, double angle) {
-            this.topSpeed = topSpeed;
-            this.bottomSpeed = bottomSpeed;
-            this.angle = angle;
-        }
 
         public State(double topSpeed, double bottomSpeed) {
-            this(topSpeed, bottomSpeed, 0);
+            this.topSpeed = topSpeed;
+            this.bottomSpeed = bottomSpeed;
         }
 
         public State(double topSpeed) {
-            this(topSpeed, topSpeed * Constants.Shooter.kDefaultBottomSpeedMultiplier, 0);
+            this(topSpeed, topSpeed * Constants.Shooter.kDefaultBottomSpeedMultiplier);
         }
 
         public String toString() {
-            return "ShooterState(topSpeed=" + topSpeed + ", bottomSpeed=" + bottomSpeed + ", angle=" + angle + ")";
+            return "ShooterState(topSpeed=" + topSpeed + ", bottomSpeed=" + bottomSpeed + ")";
         }
 
         public boolean equals(Object o) {
             if (o instanceof State) {
                 State other = (State) o;
-                return topSpeed == other.topSpeed && bottomSpeed == other.bottomSpeed && angle == other.angle;
+                return topSpeed == other.topSpeed && bottomSpeed == other.bottomSpeed;
             }
             return false;
         }
@@ -52,8 +45,7 @@ public class Shooter extends SubsystemBase {
                 State other = (State) o;
                 return
                     Utils.roughlyEqual(topSpeed, other.topSpeed, Constants.Shooter.kTopSpeedTolerance)
-                 && Utils.roughlyEqual(bottomSpeed, other.bottomSpeed, Constants.Shooter.kBottomSpeedTolerance)
-                 && Utils.roughlyEqual(angle, other.angle, Constants.Shooter.kAngleTolerance);
+                 && Utils.roughlyEqual(bottomSpeed, other.bottomSpeed, Constants.Shooter.kBottomSpeedTolerance);
             }
             return false;
         }
@@ -62,9 +54,6 @@ public class Shooter extends SubsystemBase {
     private static Shooter instance;
     private final WPI_TalonFX mBottomShooter  = new WPI_TalonFX(Constants.Shooter.kLeftShooterID);
     private final WPI_TalonFX mTopShooter = new WPI_TalonFX(Constants.Shooter.kRightShooterID);
-    // private final WPI_TalonFX mHood = new WPI_TalonFX(Constants.Shooter.kAngleShooterID);
-    private boolean isHomed;
-    private static Command mHomeCommand = new Home();
 
     public static Shooter getInstance() {
         if (instance == null) {
@@ -76,11 +65,9 @@ public class Shooter extends SubsystemBase {
     private Shooter() {
         mBottomShooter.configFactoryDefault();
         mTopShooter.configFactoryDefault();
-        // mHood.configFactoryDefault();
 
         mBottomShooter.configAllSettings(Constants.Shooter.kBottomShooterConfig);
         mTopShooter.configAllSettings(Constants.Shooter.kTopShooterConfig);
-        // mHood.configAllSettings(Constants.Shooter.kHoodConfig);
 
         mBottomShooter.setStatusFramePeriod(StatusFrameEnhanced.Status_1_General, 255);
         mBottomShooter.setStatusFramePeriod(StatusFrameEnhanced.Status_2_Feedback0, 255);
@@ -93,42 +80,12 @@ public class Shooter extends SubsystemBase {
         mBottomShooter.setStatusFramePeriod(StatusFrameEnhanced.Status_14_Turn_PIDF1, 255);
         //21?
         mBottomShooter.setStatusFramePeriod(StatusFrameEnhanced.Status_Brushless_Current, 255);
-
-        // mHood.setNeutralMode(NeutralMode.Brake);
-
-        SmartDashboard.putNumber("LeftVelo", 0.0);
-        SmartDashboard.putNumber("RightVelo", 0.0);
-        SmartDashboard.putNumber("HoodAngle", 0.0);
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
         builder.addStringProperty("Shooter State", () -> getState().toString(), null);
-        // builder.addBooleanProperty("Hood homed", () -> isHomed, null);
-    }
-
-    @Override
-    public void periodic() {
-        super.periodic();
-        // if (mHood.hasResetOccurred()) {
-        //     isHomed = false;
-        // }
-        // if (!isHomed && !mHomeCommand.isScheduled()) {
-        //     mHomeCommand.schedule();
-        // }
-
-        // SmartDashboard.putNumber(
-        //     "Distance Limelight",
-        //     Limelight.getDistance());
-        // setState(
-        //     new State(
-        //         (SmartDashboard.getNumber("TopVelo", 0)), 
-        //         (SmartDashboard.getNumber("BottomVelo", 0)), 
-        //         (SmartDashboard.getNumber("HoodAngle", 0)) 
-        //     )
-        // );
-        // SmartDashboard.putString("Table Value", "addEntry(" + Utils.round(Limelight.getDistance(), 2) + ", " + SmartDashboard.getNumber("TopVelo", 0) + ", " + SmartDashboard.getNumber("BottomVelo", 0) + ", " + SmartDashboard.getNumber("HoodAngle", 0) + ");");
     }
 
     public void setBottomShooterPower(double power) {
@@ -139,7 +96,7 @@ public class Shooter extends SubsystemBase {
         if(velocity < 500) {
             mBottomShooter.set(ControlMode.PercentOutput, 0);
         } else {
-            mBottomShooter.set(ControlMode.Velocity, (-velocity / (600.0 / 2048.0)));
+            mBottomShooter.set(ControlMode.Velocity, -velocity / (600.0 / 2048.0));
         }
     }
 
@@ -149,63 +106,21 @@ public class Shooter extends SubsystemBase {
 
     public void setTopShooterVelocity(double velocity) {
         if(velocity < 500) {
-            setTopShooterPower(0);
+            mTopShooter.set(ControlMode.PercentOutput, 0);
         } else {
-            mTopShooter.set(ControlMode.Velocity, (velocity / (600.0 / 2048.0)));
+            mTopShooter.set(ControlMode.Velocity, velocity / (600.0 / 2048.0));
         }
-    }
-
-    public void setHoodPositionPID(double position) {
-        // if(position == 0) {
-        //     setHoodPower(0);
-        // } else {
-        //     mHood.set(ControlMode.Position, position * (-8192 / 90), DemandType.ArbitraryFeedForward, 0.04);
-        // }
-    }
-
-    public void setHoodPower(double power) {
-        // mHood.set(ControlMode.PercentOutput, power);
     }
 
     public State getState() {
         return new State(
             mTopShooter.getSelectedSensorVelocity() * (600/2048f),
-            mBottomShooter.getSelectedSensorVelocity() * (600/2048f),
-            // mHood.getSelectedSensorPosition());
-            0);
+            mBottomShooter.getSelectedSensorVelocity() * (600/2048f));
     }
 
     public void setState(State state) {
         SmartDashboard.putString("Requested state", state.toString());
         setTopShooterVelocity(state.topSpeed);
         setBottomShooterVelocity(state.bottomSpeed);
-        // setHoodPositionPID(state.angle);
-    }
-
-    public void setSoftLimitEnable(boolean value) {
-        // mHood.configForwardSoftLimitEnable(value);
-        // mHood.configReverseSoftLimitEnable(value);
-    }
-
-    public void setClearPosition(boolean value) {
-        // mHood.configClearPositionOnLimitF(value, 100);
-    }
-
-    public void setMaxSpeed(double speed) {
-        // mHood.configPeakOutputForward(speed);
-        // mHood.configPeakOutputReverse(-2 * speed);
-    }
-
-    public boolean getForwardLimit() {
-        // return mHood.isFwdLimitSwitchClosed() == 1;
-        return true;
-    }
-
-    public void setHomed(boolean value) {
-        isHomed = value;
-    }
-
-    public void resetHoodEncoder() {
-        // mHood.setSelectedSensorPosition(0);
     }
 }
