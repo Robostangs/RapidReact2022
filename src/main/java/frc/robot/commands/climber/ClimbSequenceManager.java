6f724d7bdc75e4c39b42d40691ff245ac3bacb51
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-03-28 20:56:52 -0400

    Add all transitions entries

--- src/main/java/frc/robot/commands/climber/ClimbSequenceManager.java
+++ src/main/java/frc/robot/commands/climber/ClimbSequenceManager.java
@@ -7,8 +7,12 @@ import java.util.function.Supplier;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.Constants;
 import frc.robot.commands.turret.Protect;
 import frc.robot.subsystems.Climber;
@@ -34,14 +38,24 @@ public class ClimbSequenceManager implements Sendable {
     private class Transition {
         private CommandBase behavior;
         private Supplier<ClimbState> determineNextState;
+        private boolean autoSchedule = false;
 
-        public Transition(CommandBase behavior, Supplier<ClimbState> determineNextState) {
+        public Transition(CommandBase behavior, Supplier<ClimbState> determineNextState, boolean autoSchedule) {
             this.behavior = behavior;
             this.determineNextState = determineNextState;
+            this.autoSchedule = autoSchedule;
         }
 
+        private Transition(CommandBase behavior, ClimbState nextState, boolean autoSchedule) {
+            this(behavior, () -> nextState, autoSchedule);
+        }
+
+        public Transition(CommandBase behavior, Supplier<ClimbState> determineNextState) {
+            this(behavior, determineNextState, false);
+        }
+        
         private Transition(CommandBase behavior, ClimbState nextState) {
-            this(behavior, () -> nextState);
+            this(behavior, nextState, false);
         }
     }
 
@@ -55,29 +69,27 @@ public class ClimbSequenceManager implements Sendable {
         mHandA = hands[0];
         mHandB = hands[0];
     }
-    private final Map<ClimbState, Transition> mNextCommands = new HashMap<ClimbState, Transition>() {{
+    private final Map<ClimbState, Transition> mTransitions = new HashMap<ClimbState, Transition>() {{
         put(ClimbState.kStarting, new Transition(new ClimbPrep(), ClimbState.kCallibrated));
         put(ClimbState.kCallibrated, new Transition(new Protect().andThen(new ReleaseElevator()).withName("Lift climber"), ClimbState.kHigh));
         put(ClimbState.kTall, new Transition(new RotateToPosition(Constants.Climber.Rotator.kStartingAngle).withName("Rotate to Mid Bar"), ClimbState.kPreppedMid));
         put(ClimbState.kPreppedMid, new Transition(new DriveToMidBar(), ClimbState.kGrabbingMid));
         put(ClimbState.kGrabbingMid, new Transition(new CloseHand(mHandA).withName("Grab Mid Bar"), ClimbState.kMid));
-        put(ClimbState.kPreppedHigh, new Transition(new GrabNextBar(mHandB).withName("Grab High Bar"), ClimbState.kMidHigh));
-    }};
-    private final Map<ClimbState, Transition> mAutoCommands = new HashMap<ClimbState, Transition>() {{
-        put(ClimbState.kMid, new Transition(new SetHandLockPosition(mHandA, Constants.Climber.Hand.kClawLockUnlockedPositon), ClimbState.kPreppedHigh));
+        put(ClimbState.kMid, new Transition(new SetHandLockPosition(mHandA, Constants.Climber.Hand.kClawLockUnlockedPositon), ClimbState.kPreppedHigh, true));
+        put(ClimbState.kPreppedHigh, new Transition(new GrabNextBar(mHandB).withName("Grab High Bar"), () -> mHandB.getEngaged() ? ClimbState.kMidHigh : ClimbState.kMid));
+        put(ClimbState.kMidHigh, new Transition(new ParallelDeadlineGroup(new OpenHand(mHandA).andThen(new PrintCommand("Let go mid done")), new RotateWithWiggle(Constants.Climber.Rotator.kCGHoldSpeed, mWiggleSupplier)), ClimbState.kHigh));
+        put(ClimbState.kHigh, new Transition(new ParallelCommandGroup(new SetHandLockPosition(mHandA, Constants.Climber.Hand.kClawLockLockedPositon), new SetHandLockPosition(mHandB, Constants.Climber.Hand.kClawLockUnlockedPositon)), ClimbState.kPreppedTraversal));
+        put(ClimbState.kPreppedTraversal, new Transition(new GrabNextBar(mHandA).withName("Grab Traversal Bar"), () -> mHandA.getEngaged() ? ClimbState.kHighTraversal : ClimbState.kHigh));
+        put(ClimbState.kHighTraversal, new Transition(new ParallelDeadlineGroup(new OpenHand(mHandB).andThen(new PrintCommand("Let go high done")), new RotateWithWiggle(Constants.Climber.Rotator.kCGHoldSpeed, mWiggleSupplier)), ClimbState.kTraversal));
     }};
     private ClimbState mCurrentState = ClimbState.kStarting;
-    private CommandBase waitlistedCommand;
+    private Transition mCurrentTransition;
+    private Supplier<Double> mWiggleSupplier;
 
     @Override
     public void initSendable(SendableBuilder builder) {
-        builder.addStringProperty("ClimbSequence State", () -> getState().toString(), null);
-        builder.addStringProperty("Waitlisted Command", () -> waitlistedCommand != null ? waitlistedCommand.toString() : "None", null);
-        builder.addStringProperty("Next Command", () -> getNextCommand() != null ? getNextCommand().toString() : "None", null);
-    }
-
-    public void periodic() {
-
+        builder.addStringProperty("ClimbSequence State", () -> mCurrentState.toString(), null);
+        builder.addStringProperty("ClimbSequence Command", () -> mCurrentTransition != null ? mCurrentTransition.behavior.getName() : "", null);
     }
 
     public static ClimbSequenceManager getInstance() {
@@ -89,33 +101,35 @@ public class ClimbSequenceManager implements Sendable {
 
     private ClimbSequenceManager() {
         SendableRegistry.addLW(this, "Climber", "ClimbSequenceManager");
+        mCommandScheduler.onCommandFinish(this::checkForTransitionFinish);
     }
 
-    void setState(ClimbState newState) {
-        mCurrentState = newState;
-    }
-
-    ClimbState getState() {
-        return mCurrentState;
-    }
-
-    CommandBase getNextCommand() {
-        return mNextCommands.get(getState()).behavior;
-    }
-
-    void waitlist(CommandBase nextCommand) {
-        if(nextCommand != null) {
-
+    private void checkForTransitionFinish(Command command) {
+        if(command == mCurrentTransition.behavior) {
+            advanceState();
         }
     }
 
-    public void advance() {
+    public void setWiggleSupplier(Supplier<Double> wiggleSupplier) {
+        mWiggleSupplier = wiggleSupplier;
+    }
+
+    public void proceed() {
         if(mCommandScheduler.requiring(mClimber) != null) {
             interrupt();
         }
     }
 
     public void interrupt() {
-        getNextCommand().schedule();
+        mCurrentTransition.behavior.cancel();
+        advanceState();
+    }
+
+    private void advanceState() {
+        mCurrentState = mCurrentTransition.determineNextState.get();
+        mCurrentTransition = mTransitions.get(mCurrentState);
+        if (mCurrentTransition.autoSchedule) {
+            mCommandScheduler.schedule(mCurrentTransition.behavior);
+        }
     }
 }
