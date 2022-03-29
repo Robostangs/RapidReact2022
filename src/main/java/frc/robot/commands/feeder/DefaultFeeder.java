4478157389cd29696225b56f80138b9fcb4622da
RCoder01 <45217840+RCoder01@users.noreply.github.com>
2022-03-29 02:11:49 -0400

    Merge branch 'main' into Climber-Rework

--- src/main/java/frc/robot/commands/feeder/DefaultFeeder.java
+++ src/main/java/frc/robot/commands/feeder/DefaultFeeder.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.feeder;
 
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.util.WPIUtilJNI;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Feeder;
@@ -7,6 +9,8 @@ import frc.robot.subsystems.Feeder;
 public class DefaultFeeder extends CommandBase {
 
     private final Feeder mFeeder = Feeder.getInstance();
+    private final Debouncer inDebouncer = new Debouncer(Constants.Feeder.kInDebounceTime);
+    private final Debouncer outDebouncer = new Debouncer(Constants.Feeder.kOutDebounceTime);
 
     public DefaultFeeder() {
         addRequirements(mFeeder);
@@ -15,10 +19,10 @@ public class DefaultFeeder extends CommandBase {
 
     @Override
     public void execute() {
-        if (mFeeder.getIntakeSensorLight() && !mFeeder.getShooterSensorLight()) {
-            mFeeder.moveBelt(Constants.Feeder.kSlowBeltSpeed);
-        } else {
+        if (outDebouncer.calculate(mFeeder.getShooterSensorLight() || !mFeeder.getShooterSensorDark())) {
             mFeeder.moveBelt(0);
+        } else if (inDebouncer.calculate(mFeeder.getIntakeSensorLight() || !mFeeder.getIntakeSensorDark())) {
+            mFeeder.moveBelt(Constants.Feeder.kBeltSpeed);
         }
     }
 }
