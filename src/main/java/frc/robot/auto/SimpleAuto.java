6d81f436f3a5976257bbc8787206245ee40a6864
548Software <software@robostangs.com>
2022-04-01 14:21:12 -0400

    Use power offset during backwards auton

--- src/main/java/frc/robot/auto/SimpleAuto.java
+++ src/main/java/frc/robot/auto/SimpleAuto.java
@@ -26,7 +26,7 @@ public class SimpleAuto extends SequentialCommandGroup {
             new InstantCommand(() -> 
                 mDrivetrain.drivePower(0.2 * Constants.Drivetrain.kPowerOffsetMultiplier, -0.2)),
             new Active(0.5).withTimeout(2.5),
-            new InstantCommand(() -> mDrivetrain.drivePower(-0.2, 0.2)),
+            new InstantCommand(() -> mDrivetrain.drivePower(-0.2 * Constants.Drivetrain.kPowerOffsetMultiplier, 0.2)),
             new WaitCommand(1.5),
             new InstantCommand(() -> mDrivetrain.drivePower(0, 0)),
 
